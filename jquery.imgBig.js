8cd15c2df525ba15cbfd19a6371c3807a768aa81
刘博威 <35991126+LiuBoWei8386@users.noreply.github.com>
2018-11-15 19:59:37 +0800

    jquery插件

---
+++ jquery.imgBig.js
@@ -0,0 +1,30 @@
+;(function($){
+	$.fn.imgBig=function(boxO,imgO){
+		//console.log("this:",this);
+		var box={
+			width: "300px",
+			height: "300px",
+			border: "1px solid red",
+			borderRadius: "100%"
+		}
+		var img={
+			speed:2,
+			scale:1.5
+		}
+		var boxSet=$.extend(true,{},box, boxO);
+		var imgSet=$.extend(true,{},img, imgO);
+		
+		this.css(boxSet).children("img").css({
+				"transition":imgSet.speed+"s"
+		})
+		this.hover(function(){
+			$(this).children("img").css({
+				transform:"scale("+imgSet.scale+")",
+				cursor:"pointer"
+			});
+		},function(){
+			$(this).children("img").css("transform","scale(1)");
+		})
+		return this;
+	}
+}(jQuery));
